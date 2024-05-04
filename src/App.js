33ef381936af93246a2d80126ba900f3e1b6bab0
Ashish Kumar Pal <156321297+AshishKumarPaul@users.noreply.github.com>
2024-05-04 18:11:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import appConfig from './app.config';
 import About from './components/About/About';
 import Articles from './components/Articles/Articles';
 import Nav from './components/Nav/Nav';
-import Footer from './components/Footer/footer';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
